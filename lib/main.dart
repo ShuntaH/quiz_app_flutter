13f88bafab74d2b106673427f8a5f2301dcbb757
ShuntaH <exec.apea@gmail.com>
2022-11-01 21:32:23 +0900

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
-  var questions = ['好きなラーメンは？', '好きなパスタは？', '好きな寿司は？'];
   var questionIndex = 0;
 
   void answerQuestion() {
@@ -24,16 +23,19 @@ class MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    var questions = ['好きなラーメンは？', '好きなパスタは？', '好きな寿司は？'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('初めてのflutter'),
         ),
         body: Column(children: const [
-          Text('the question'),
-          ElevatedButton(onPressed: null, child: Text('answer1')),
-          ElevatedButton(onPressed: null, child: Text('answer1')),
-          ElevatedButton(onPressed: null, child: Text('answer1')),
+          Text(
+            questions[questionIndex],
+          ),
+          ElevatedButton(onPressed: answerQuestion, child: Text('answer1')),
+          ElevatedButton(onPressed: answerQuestion, child: Text('answer1')),
+          ElevatedButton(onPressed: answerQuestion, child: Text('answer1')),
         ]),
       ),
     );
